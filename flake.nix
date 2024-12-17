f9cbcb053f668c8289e719eeaabf152c7b79659f
Jared Ramirez <jaredpramirez@proton.me>
2024-12-17 12:51:39 -0800

    WIP

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
-  description = "High-performance, multiplayer code editor from the creators of Atom and Tree-sitter";
+  description =
+    "High-performance, multiplayer code editor from the creators of Atom and Tree-sitter";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
@@ -9,27 +10,21 @@
     };
     crane.url = "github:ipetkov/crane";
     flake-compat.url = "github:edolstra/flake-compat";
+
+    # Needed until swift 6 is available in nixpkgs
+    swift_6.url = "github:timothyklim/swift-flake";
   };
 
-  outputs =
-    {
-      nixpkgs,
-      rust-overlay,
-      crane,
-      ...
-    }:
+  outputs = { nixpkgs, rust-overlay, crane, swift_6, ... }:
     let
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
+      systems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
       overlays = {
         rust-overlay = rust-overlay.overlays.default;
         rust-toolchain = final: prev: {
-          rustToolchain = final.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          rustToolchain =
+            final.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         };
         zed-editor = final: prev: {
           zed-editor = final.callPackage ./nix/build.nix {
@@ -37,31 +32,46 @@
             rustToolchain = final.rustToolchain;
           };
         };
+        swift = final: prev: {
+          swift = swift_6.packages.${prev.system}.default;
+        };
       };
 
-      mkPkgs =
-        system:
+      mkPkgs = system:
         import nixpkgs {
           inherit system;
           overlays = builtins.attrValues overlays;
         };
 
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (mkPkgs system));
-    in
-    {
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system: f (mkPkgs system));
+    in {
       packages = forAllSystems (pkgs: {
         zed-editor = pkgs.zed-editor;
         default = pkgs.zed-editor;
       });
 
-      devShells = forAllSystems (pkgs: {
-        default = import ./nix/shell.nix { inherit pkgs; };
-      });
+      devShells = forAllSystems (pkgs:
+        let craneLib = crane.mkLib pkgs;
+        in {
+          default = craneLib.devShell {
+            # Automatically inherit any build inputs from `zed-editor`
+            inputsFrom = [ pkgs.zed-editor ];
+
+            # Any additional dev deps
+            packages = [ pkgs.rust-analyzer ];
+
+            # Set any necessary env variables
+            BINDGEN_EXTRA_CLANG_ARGS = "--sysroot=$(xcrun --show-sdk-path)";
+            BUILD_LIBRARY_FOR_DISTRIBUTION = "YES";
+          };
+        });
 
       formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
 
       overlays = overlays // {
-        default = nixpkgs.lib.composeManyExtensions (builtins.attrValues overlays);
+        default =
+          nixpkgs.lib.composeManyExtensions (builtins.attrValues overlays);
       };
     };
 }
